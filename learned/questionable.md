0dbc11c21bac0de1f1cfaabc633a93ea01a12930
jaesoon <bonrivers1@gmail.com>
2022-07-05 17:05:38 +0900

    20220705 공부 추가됨

---
+++ learned/questionable.md
@@ -0,0 +1,4 @@
+# 궁금한 것들
+
+- static 이란 무엇인가?
+- :memo: [2022-07-05](https://github.com/Verdemese/typescript-study/blob/main/learned/20220705.md)
--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -5,7 +5,6 @@ let duplicate = [...container];
 
 const button = document.querySelector("button")!;
 
-
 function checkAnalytics(someData: string) {
     console.log(someData);
 }
@@ -13,7 +12,73 @@ function checkAnalytics(someData: string) {
 checkAnalytics("string data");
 
 function clickHandler(message: string) {
-    console.log('clicked! ' + message);
+    console.log("clicked! " + message);
 }
 
-button.addEventListener("click", clickHandler.bind(null));
+button.addEventListener(
+    "click",
+    clickHandler.bind(null, "clicked with the binded function")
+);
+
+class StaticClass {
+    private name: string;
+    private static staticNumber: number = 1;
+
+    constructor() {
+        this.name = "test";
+    }
+
+    addStaticNumber(): void {
+        StaticClass.staticNumber = StaticClass.staticNumber + 1;
+    }
+
+    getStaticNumber(): number {
+        return StaticClass.staticNumber;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getPrivateStaticNumber(): number {
+        return StaticClass.staticNumber;
+    }
+
+    static getPrivateStaticNumberWithStaticfunction(): number {
+        return StaticClass.staticNumber;
+    }
+}
+
+const class1 = new StaticClass();
+const class2 = new StaticClass();
+const class3 = new StaticClass();
+
+class1.addStaticNumber();
+class2.addStaticNumber();
+class3.addStaticNumber();
+
+console.log(class3.getPrivateStaticNumber());
+
+console.log(StaticClass.getPrivateStaticNumberWithStaticfunction());
+
+
+class ShortHand {
+    constructor (private readonly id:number, public name:string) {
+
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(): void {
+        // this.id = 2;
+    }
+
+}
+
+const short = new ShortHand(1, 'verde');
+
+console.log(short.getId());
+console.log(short.name);
+

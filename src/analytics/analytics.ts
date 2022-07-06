6e230bfb2de4b2628038e101fa01f93c7553510b
jaesoon <bonrivers1@gmail.com>
2022-07-06 17:50:14 +0900

    20220706 공부 추가됨

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -61,32 +61,72 @@ console.log(class3.getPrivateStaticNumber());
 console.log(StaticClass.getPrivateStaticNumberWithStaticfunction());
 
 class ShortHand {
-    constructor(private readonly id: number, public name: string) {}
+    protected employees: string[] = ['verde', 'jaesoon'];
 
-    getId() {
+    constructor(
+        private readonly id: number,
+        public name: string
+    ) {}
+
+    getId(): number {
         return this.id;
     }
 
-    setId(): void {
-        // this.id = 2;
+    setName(name: string): void {
+        this.name = name;
+    }
+
+    setEmployees(name: string): void {
+        this.employees.push(name);
     }
 }
 
 const short = new ShortHand(1, "verde");
 
+short.setName('jaesoon');
+
 console.log(short.getId());
 console.log(short.name);
 
 class ITDepartment extends ShortHand {
+    private lastReport: string = 'The climate has been changed a lot by human beings';
+
     constructor(id: number, name: string, private admins: string[]) {
         super(id, name);
     }
 
-    printAdmins() {
+    setName(): void {
+        this.name = "verde";
+    }
+
+    printAdmins(): void {
         console.log(this.admins);
+    }
+    
+    printEmployees() {
+        console.log(this.employees);
+    }
+
+    get recentReport() {
+        if (!!this.lastReport) {
+            return this.lastReport;
+        }
+
+        throw new Error('There is no recent report');
+    }
+
+    set recentReport(value: string) {
+        if (!value) throw new Error('Please pass valid value');
+
+        this.lastReport = value;
     }
 }
 
 const department = new ITDepartment(1, "verde", ["verde", "boosee"]);
 
 department.printAdmins();
+department.setName();
+department.printEmployees();
+
+department.recentReport = 'valid value';
+console.log(department.recentReport);
